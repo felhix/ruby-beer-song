39e3f8d3d491e576c4ffc341914bc61dfba22d96
felhix <felhix@hotmail.com>
2017-09-13 20:18:10 +0200

    3 tests qui marchent

--- beer_song.rb
+++ beer_song.rb
@@ -1,8 +1,19 @@
-class BeerSong
 
+class BeerSong
   def verse(n)
-    "#{n.to_s} bottles of beer on the wall, 99 bottles of beer.\n" \
-    "Take one down and pass it around, #{(n - 1).to_s} bottles of beer on the wall.\n"
+    "#{n.to_s} bottles of beer on the wall, #{n.to_s} bottles of beer.\n" \
+    "Take one down and pass it around, #{bottles_pluralized(n - 1)} of beer on the wall.\n"
   end
+
+
+  private
+
+    def bottles_pluralized(count)
+      if count == 1
+        "#{count} bottle"
+      else
+        "#{count} bottles"
+      end
+    end
 
 end
